5c1aa37882014d79631901e67510b1b92f65a912
Jason <jikurata@gmail.com>
2019-03-02 21:39:31 -0800

    update readme

---
+++ README.md
@@ -0,0 +1,55 @@
+# Taste v0.0.0
+Test client-side code directly in a web browser
+---
+## Install
+```
+npm install @jikurata/taste
+```
+## Usage
+Taste utilizes familiar syntaxes from Jest and Mocha.
+```
+const Taste = require('@jikurata/taste');
+const taste = new Taste();
+
+// test these
+function add(x,y) { return x+y; }
+function div(el) { el.appendChild(document.createElement('div')); }
+
+taste.describe('Returns the sum of two numbers', () => {
+    taste.describe('Returns 8', () => {
+        const sum = add(3,5);
+        taste.expect(sum).tobe(8);
+    });
+});
+taste.describe('Appends a div element to its target', () => {
+    taste.describe('target has one div element', () => {
+        const doc = taste.sample(`
+            <section id="foo"></section>
+        `);
+        const target = doc.getElementById('foo');
+        div(target);
+        taste.expect(target.getElementsByTagName('div').length).toBe(1);
+    });
+});
+```
+Testing asynchronous code is no problem
+```
+function asyncFn() {
+    window.setTimeout(() => {
+        taste.expect(1).toBe(1);
+    }, 6000);
+}
+
+taste.test('Resolves test in 6000ms', () => {
+    asyncFn(); // test resolves after timeout fn triggers
+    taste.timeout(7000); // Change test timeout to 7000ms to prevent the test from throwing before asyncFn is called. Default timeout is 5000ms
+});
+```
+## Plans
+- Make compatible with Internet Explorer
+- Implement more resolution methods for expect
+- Implement before and after
+- Improve organization of the test view
+
+## Version Log
+---
--- _test/bundle.test.js
+++ _test/bundle.test.js
@@ -13,51 +13,48 @@ function appendDiv(target) {
 
 function run() {
   const taste = new Taste();
-  const root = document.getElementById('test');
-  taste.prepare(root, () => {
-    taste.describe('This runs an asynchronous test for 7 seconds', () => {
-      taste.test('This test sets the timeout to 8000ms to pass the test before timeout', () => {
-        window.setTimeout(() => {
-          taste.expect(1).toBe(1);
-        }, 7000);
-        taste.timeout(8000);
-      });
+  taste.describe('This runs an asynchronous test for 7 seconds', () => {
+    taste.test('This test sets the timeout to 8000ms to pass the test before timeout', () => {
+      window.setTimeout(() => {
+        taste.expect(1).toBe(1);
+      }, 7000);
+      taste.timeout(8000);
     });
-    taste.describe('Adds two numbers together', () => {
-      taste.test('Add(3,5) returns 8', () => {
-        taste.expect(add(3,5)).toBe(8);
-      });
-      taste.test('Add(-3,5) returns 2', () => {
-        taste.expect(add(-3,5)).toBe(2);
-      });
+  });
+  taste.describe('Adds two numbers together', () => {
+    taste.test('Add(3,5) returns 8', () => {
+      taste.expect(add(3,5)).toBe(8);
     });
-    taste.describe('This test uses sample to create a dom tree for the test', () => {
-      const doc = taste.sample(`
-        <section id="test">
-          <p>0</p>
-        </section>
-      `);
-      taste.test('Target has one div element', () => {
-        const node = doc.getElementById('test');
-        appendDiv(node);
-        taste.expect(doc.getElementsByTagName('div').length).toBe(1);
-      });
+    taste.test('Add(-3,5) returns 2', () => {
+      taste.expect(add(-3,5)).toBe(2);
     });
-    taste.describe('This test uses sample to create a dom tree for the test', () => {
-      const doc = taste.sample(`
-        <section id="test">
-          <p>1</p>
-        </section>
-      `);
-      taste.test('Sample creates a separate context from the previous sample', () => {
-        const node = doc.getElementById('test');
-        taste.expect(node.textContent.trim()).toBe(1);
-      });
+  });
+  taste.describe('This test uses sample to create a dom tree for the test', () => {
+    const doc = taste.sample(`
+      <section id="test">
+        <p>0</p>
+      </section>
+    `);
+    taste.test('Target has one div element', () => {
+      const node = doc.getElementById('test');
+      appendDiv(node);
+      taste.expect(doc.getElementsByTagName('div').length).toBe(1);
     });
-    taste.describe('This test is designed to fail', () => {
-      taste.test('Returns 1', () => {
-        taste.expect(0).toBe(1);
-      });
+  });
+  taste.describe('This test uses sample to create a dom tree for the test', () => {
+    const doc = taste.sample(`
+      <section id="test">
+        <p>1</p>
+      </section>
+    `);
+    taste.test('Sample creates a separate context from the previous sample', () => {
+      const node = doc.getElementById('test');
+      taste.expect(node.textContent.trim()).toBe(1);
+    });
+  });
+  taste.describe('This test is designed to fail', () => {
+    taste.test('Returns 1', () => {
+      taste.expect(0).toBe(1);
     });
   });
   taste.runTests();
@@ -522,7 +519,7 @@ class Test extends Context {
       <p class="title">${this.description}</p>
       <ul class="info">
         <p>Progress: <span class="progress">In queue</span></p>
-        <p>Duration: <span class="duration">0</span>ms</p>
+        <p>Duration: <span class="duration"></span>ms</p>
         <p class="source"></p>
       </ul>
     `;
