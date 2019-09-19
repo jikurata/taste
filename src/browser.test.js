02c9e30d59f9627ef51bc76706bc07cdaaa5da38
Jason <jikurata@gmail.com>
2019-09-19 01:36:06 -0700

    Flavors implement Profile

--- src/browser.test.js
+++ src/browser.test.js
@@ -1,6 +1,6 @@
 'use strict';
 const Taste = require('../index.js');
-const Flavor = require('../lib/Flavor.js');
+const Profile = require('../lib/Profile.js');
 
 function add(x,y) {
   return x + y;
@@ -9,65 +9,61 @@ function add(x,y) {
 function appendDiv(target) {
   const node = document.createElement('div');
   target.appendChild(node);
-}
+} 
 
 Taste.prepare('#test');
 
 Taste.flavor('Synchronous pass test')
-  .describe('Add 4 + 1')
-  .test(() => {
-    Taste.profile.addResult = add(4,1);
-    Taste.profile.addResultAgain = add(6,4);
-  })
-  .expect('addResult').toEqual(5)
-  .expect('addResultAgain').toEqual(10);
+.describe('Add 4 + 1')
+.test(profile => {
+  profile.addResult = add(4,1);
+  profile.addResultAgain = add(6,4);
+})
+.expect('addResult').toEqual(5)
+.expect('addResultAgain').toEqual(10);
 
 Taste.flavor('Synchronous fail test')
-  .describe('Add 4 + 1')
-  .test(() => {
-    Taste.profile.wrongResult = add(4,1);
-  })
-  .expect('wrongResult').toEqual(3);
+.describe('Add 4 + 1')
+.test(profile => {
+  profile.wrongResult = add(4,1);
+})
+.expect('wrongResult').toEqual(3);
 
 Taste.flavor('Asynchronous pass test')
-  .timeout(5000)
-  .describe('Resolves after 3000ms')
-  .test(() => {
-    setTimeout(() => {
-      Taste.profile.asyncResult = true;
-    }, 3000);
-  })
-  .expect('asyncResult').toBeTruthy();
+.timeout(5000)
+.describe('Resolves after 3000ms')
+.test(profile => {
+  setTimeout(() => {
+    profile.asyncResult = true;
+  }, 3000);
+})
+.expect('asyncResult').toBeTruthy();
 
 Taste.flavor('Asynchronous fail test')
-  .timeout(5000)
-  .describe('Fails after 3000ms')
-  .test(() => {
-    setTimeout(() => {
-      Taste.profile.asyncFailed = true;
-    }, 3000);
-  })
-  .expect('asyncFailed').toBeFalsy();
+.timeout(5000)
+.describe('Fails after 3000ms')
+.test(profile => {
+  setTimeout(() => {
+    profile.asyncFailed = true;
+  }, 3000);
+})
+.expect('asyncFailed').toBeFalsy();
 
 Taste.flavor('Asynchronous timeout test')
-  .describe('Test exceeds timeout')
-  .test(() => {
-    setTimeout(() => {
-      Taste.profile.asyncTimeout = true;
-    }, 3000);
-  })
-  .expect('asyncTimeout').toBeTruthy();
-
-Taste.flavor('Pass Flavor instance to test')
-.describe('this is the current Flavor in the test scope')
-.test((flavor) => {
-  console.log('flavor', flavor, this);
-  Taste.profile.thisIsFlavor = this instanceof Flavor;
+.describe('Test exceeds timeout')
+.test(profile => {
+  setTimeout(() => {
+    profile.asyncTimeout = true;
+  }, 3000);
 })
-.expect('thisIsFlavor').toBeTruthy()
-.finished((flavor) => {
-  console.log('done', flavor);
-});
+.expect('asyncTimeout').toBeTruthy();
+
+Taste.flavor('Pass Flavor profile instance to test')
+.describe('profile is the current Flavor profile in the test scope')
+.test(profile => {
+  profile.thisIsFlavor = profile instanceof Profile;
+})
+.expect('thisIsFlavor').toBeTruthy();
   
 if ( Taste.isBrowser ) {
   Taste.flavor('Taste sample dom test')
@@ -77,9 +73,9 @@ if ( Taste.isBrowser ) {
       <p>Sample Html Test</p>
     </section>
   `)
-  .test((sample) => {
+  .test((profile, sample) => {
     sample.innerHTML += '<p>This text was added during the test.</p>';
-    Taste.profile.childrenLength = sample.children.length;
+    profile.childrenLength = sample.children.length;
   })
   .expect('childrenLength').toBe(2);
 
